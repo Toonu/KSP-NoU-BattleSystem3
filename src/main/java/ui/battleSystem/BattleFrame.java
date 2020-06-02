dff62b658ade966c8ff28d41ac29d4dba7240723
toonu <toonu44@gmail.com>
2020-06-02 13:35:19 +0200

    Battle Map WIP 0.2.2

--- src/main/java/ui/battleSystem/BattleFrame.java
+++ src/main/java/ui/battleSystem/BattleFrame.java
@@ -5,17 +5,8 @@ import impl.OOB;
 import ui.Gui;
 import ui.JCraftList;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.ScrollPane;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Toonu
@@ -27,11 +18,10 @@ public class BattleFrame extends JFrame {
     private final JCraftList<Craft> whiteListedCrafts = new JCraftList<>(textForWhite);
     private final JCraftList<Craft> blackListedCrafts = new JCraftList<>(textForBlack);
     private final JPanel crafts = new JPanel(new BorderLayout(5, 0));
-    private final BattleMap map = new BattleMap();
+    private final JPanel map = new JPanel(new GridBagLayout());
 
     public BattleFrame(String title) {
         super(title);
-
         setLayout(new GridBagLayout());
 
         c.setBackground(Gui.BACKGROUND);
@@ -49,27 +39,50 @@ public class BattleFrame extends JFrame {
         blackListedCrafts.updateUI(OOB.BLACK.getCrafts());
 
         GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         gbc.weightx = 0.9;
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weighty = 1;
+        gbc.gridheight = 2;
         mapScroll.add(map);
+
         c.add(mapScroll, gbc);
         map.repaint();
+        GridBagConstraints gbcc = new GridBagConstraints();
+        gbcc.anchor = GridBagConstraints.CENTER;
 
-        ++gbc.gridx;
-        JButton zoom = new JButton("Zoom");
-        zoom.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                map.zoom(map.getMap());
-            }
-        });
-        c.add(zoom, gbc);
+        map.add(new BattleMap(), gbcc);
 
-        ++gbc.gridx;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
         gbc.weightx = 0.1;
+        gbc.weighty = 0.95;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH;
         c.add(crafts, gbc);
 
+        gbc = new GridBagConstraints();
+        gbc.gridwidth = 1;
+        gbc.weighty = 0.05;
+        gbc.gridy = 1;
+        JButton zoomIn = new JButton("Zoom In ");
+        zoomIn.addActionListener(e -> {
+            BattleMap bm = (BattleMap) map.getComponent(0);
+            bm.zoomIn();
+            bm.updateUI();
+        });
+        c.add(zoomIn, gbc);
+
+        gbc.gridx = 2;
+        JButton zoomOut = new JButton("Zoom Out");
+        zoomOut.addActionListener(e -> {
+            BattleMap bm = (BattleMap) map.getComponent(0);
+            bm.zoomOut();
+            bm.updateUI();
+        });
+        c.add(zoomOut, gbc);
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         validate();
--- src/main/java/ui/battleSystem/BattleMap.java
+++ src/main/java/ui/battleSystem/BattleMap.java
@@ -3,13 +3,8 @@ package ui.battleSystem;
 import crafts.Craft;
 import impl.OOB;
 
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Toonu
@@ -17,39 +12,50 @@ import java.awt.image.BufferedImage;
 public class BattleMap extends JPanel {
     private Image map;
     private Graphics gr;
+    private final JPanel centeringPanel = new JPanel(new BorderLayout());
+    private double zoom = 1;
 
     public BattleMap() {
         super();
-        setPreferredSize(new Dimension(1700, 900));
+        zoom = 2;
+        setPreferredSize(new Dimension((int) Math.round(1700 * zoom), (int) Math.round(900 * zoom)));
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        paint(g);
+    }
 
+    public void paint(Graphics g) {
         gr = g;
 
         map = new ImageIcon("map.png").getImage();
 
-        gr.drawImage(map, 0, 0, null);
+        gr.drawImage(map, 0, 0, (int) Math.round(1700 * zoom), (int) Math.round(900 * zoom), null);
         gr.setColor(Color.WHITE);
         gr.drawString("Hello World", 100, 100);
         for (Craft craft : OOB.WHITE.getCrafts()) {
-            gr.drawString("x", (int) Math.round(craft.getPosition().getX()), (int) Math.round(craft.getPosition().getY()));
+            gr.drawString("x",
+                    (int) Math.round(craft.getPosition().getX() * zoom),
+                    (int) (Math.round(craft.getPosition().getY() * zoom)));
         }
         for (Craft craft : OOB.BLACK.getCrafts()) {
-            gr.drawString("y", (int) Math.round(craft.getPosition().getX()), (int) Math.round(craft.getPosition().getY()));
+            gr.drawString("y", (int) Math.round(craft.getPosition().getX() * zoom),
+                    (int) Math.round(craft.getPosition().getY() * zoom));
         }
     }
 
-    public void zoom(Image image) {
-        int newImageWidth = 1920 * 2;
-        int newImageHeight = 1080 * 2;
-        BufferedImage resizedImage = new BufferedImage(newImageWidth, newImageHeight, 1);
-        gr = resizedImage.createGraphics();
-        gr.drawImage(image, 0, 0, newImageWidth, newImageHeight, null);
-        gr.dispose();
-        this.repaint();
+    public void zoomIn() {
+        zoom *= 2;
+        System.out.println(zoom);
+        setPreferredSize(new Dimension((int) Math.round(1700 * zoom), (int) Math.round(900 * zoom)));
+    }
+
+    public void zoomOut() {
+        zoom /= 2;
+        System.out.println(zoom);
+        setPreferredSize(new Dimension((int) Math.round(1700 * zoom), (int) Math.round(900 * zoom)));
     }
 
     public Image getMap() {
