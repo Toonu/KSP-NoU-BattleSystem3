4b9633ee9e9925f61c07ef18c81d082c00a32f56
Toonu <Toonu44@gmail.com>
2020-06-09 10:48:19 +0200

    Added 1940s Enum.

--- src/main/java/enums/Era.java
+++ src/main/java/enums/Era.java
@@ -6,8 +6,8 @@ package enums;
  * enums to represent Eras and their modifiers to the craft's effectivity.
  */
 public enum Era {
-    Era1950(1), Era1960(1.2), Era1970(1.4), Era1980(1.6), Era1990(1.8), Era2000(2),
-    Era2010(2.2), Era2020(2.5);
+    Era1940(1), Era1950(1), Era1960(1.2), Era1970(1.4), Era1980(1.6), Era1990(1.8),
+    Era2000(2), Era2010(2.2), Era2020(2.5);
 
     private final double modifier;
 
